ffd6d861a43ba4be7e1acd81cbb02d3f89c78437
Haris, Syed Muhammad <muhammad.haris@afiniti.com>
2021-06-04 20:47:44 +0500

    Add bottom nav and fragments

--- app/src/main/java/com/example/duasofthequran/MainActivity.kt
+++ app/src/main/java/com/example/duasofthequran/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.duasofthequran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dailyDuaFragment = DailyDuaFragment()
+        val duaListFragment = DuaListFragment()
+        val favDuasFragment = FavouriteDuasFragment()
+        if(savedInstanceState == null) { // Initial fragment should be Daily dua
+            activateFragment(dailyDuaFragment)
+        }
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNavBar.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.nav_todays_dua -> activateFragment(dailyDuaFragment)
+                R.id.nav_all_duas -> activateFragment(duaListFragment)
+                R.id.nav_fav_duas -> activateFragment(favDuasFragment)
+            }
+            true
+        }
+    }
+
+
+    private fun activateFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main_activity, fragment)
+            .commit()
     }
 }
